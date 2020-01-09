b2636353cf4f93c46e86329b998cb14eb68576bc
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-01-09 12:36:53 -0600

    CapitalizeFirstLetter Playground

---
+++ CapitalizeFirstLetter.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func capitalizeFirstLetter(_ string: String) {
+    
+    let firstSpace = string.firstIndex(of: " ")!
+    let firstWord = string[..<firstSpace]
+
+    
+    for char in string {
+        if char == " " {
+            
+        }
+    }
+    
+}
+
+capitalizeFirstLetter("Alex is the one")
